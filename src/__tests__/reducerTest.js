6c7f7df4a1b143d322bbc5b36fe57823adab4c76
mykemk <mykemitrichev@gmail.com>
2021-04-17 14:35:09 +0200

    updated project description

--- src/__tests__/reducerTest.js
+++ src/__tests__/reducerTest.js
@@ -1,4 +1,4 @@
-import counter from 'scripts/reducer'
+import counter from 'utils/reducer'
 import expect from 'expect'
 
 test('The counter works for increament',() =>{
@@ -7,4 +7,12 @@ test('The counter works for increament',() =>{
 
 test('The counter works for decreament',() =>{
     expect(counter(0,{type:'INCREAMENT'})).toEqual(1);
+})
+
+test('Counter returns 0 for undefined state',() => {
+    expect(counter()).toEqual(0);
+})
+
+test('Counter returns the initial state for undefined type',() =>{
+    expect(counter(2,{type:"SOMETHING_ELSE"})).toEqual(2);
 })
--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -1,8 +1,13 @@
 function counter(state,action){
+    if(typeof state === 'undefined'){
+        return 0
+    }
     if(action.type === 'INCREAMENT'){
         return state + 1;
     } else if(action.type === 'DECREAMENT'){
         return state -1;
+    }else {
+        return state
     }
 }
 
